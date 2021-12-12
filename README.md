4359b75ddb6fe6af1e40d55542b88bfad9b944fe
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-12-12 15:44:13 +0300

    code is added

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/50rowe4go5mvfkff?svg=true)](https://ci.appveyor.com/project/Manchester85/patterns)
+[![Build status](https://ci.appveyor.com/api/projects/status/j9291h3xv89c1fq1?svg=true)](https://ci.appveyor.com/project/Manchester85/pageobject)
---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,25 @@
+package ru.netology.test;
+
+import lombok.var;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTest {
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+ }
+
+
